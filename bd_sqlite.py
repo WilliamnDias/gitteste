1e0f38f06b3b1c6f265afb8556c7ae94d6540033
William Dias <william_dias_05@hotmail.com>
2022-09-29 22:50:16 -0300

    bd nomes iguais

--- bd_sqlite.py
+++ bd_sqlite.py
@@ -3,11 +3,20 @@ import sqlite3
 conexao = sqlite3.connect('basededados.db')
 cursor = conexao.cursor()
 
+# cursor.execute('CREATE TABLE IF NOT EXISTS clientes('
+#                'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+#                'nome TEXT,'
+#                'peso REAL'
+#                ')')
 
+# cursor.execute('INSERT INTO clientes (nome, peso) VALUES ("William Novaes", 80.5)')
+# conexao.commit()
 
+cursor.execute('SELECT * FROM clientes')
 
-
-
+for linha in cursor.fetchall():
+    identificador, nome, peso = linha
+    print(identificador, nome, peso)
 
 
 
